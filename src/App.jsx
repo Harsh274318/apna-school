ec06e260b8f12443fe5e17c93851d17baf2a6c2d
Harsh Vardhan Pal <hp77335@gmail.com>
2026-04-18 13:32:05 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import HamBurger from './pages/landing/HamBurger';
-import { RxHamburgerMenu } from 'react-icons/rx';
 import AppRoutes from './routes/AppRoutes';
 import "./components/forms/form.css"
 import "./pages/posts.css"
